c7a9a20d63b990389b582dffbef5b52fbe78f54a
LoveHeart <113758022+LeeyooMuzic@users.noreply.github.com>
2024-05-01 14:04:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "22368569"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "11efb42a66b1d13a1ab46a3e24c3abb1"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "7029942301"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "Noob_Coderzzz"
+OWNER_ID = "6319746147"
+SUPPORT_GRP = "new_heroku_cc"
+UPDATE_CHNL = "new_heroku_cc"
+OWNER_USERNAME = "lakDVI"
 
