9a1a1bb6efc0255c46d9a2bd065ce4b8c6730147
ᴍʏʟᴏᴠᴇᴍᴏᴏɴ <90930470+MyLoveMoon@users.noreply.github.com>
2024-06-20 13:21:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "26689857"
+API_ID = "15229796"
 # -------------------------------------------------------------
-API_HASH = "b43b1c688b85941cd78a8d3972943eb3"
+API_HASH = "4e3363ea7214ebab7a03b9300bba722c"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6144662920"
+OWNER_ID = "5342963115"
 SUPPORT_GRP = "new_heroku_cc"
 UPDATE_CHNL = "new_heroku_cc"
-OWNER_USERNAME = "imHamkerCat"
+OWNER_USERNAME = ""
 
